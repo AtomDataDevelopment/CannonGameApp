c033d6e0ece39c08fd85a60a2a06a2d1beaa3891
Gustavo Junio Ferreira Rodrigues <gustavo.rodrigues@statum.com.br>
2025-11-24 21:57:18 -0300

    second commit

---
+++ app/src/main/java/br/com/prog3/cannongameapp/CannonView.java
@@ -0,0 +1,13 @@
+package br.com.prog3.cannongameapp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class CannonView extends SurfaceView {
+
+
+    public CannonView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
